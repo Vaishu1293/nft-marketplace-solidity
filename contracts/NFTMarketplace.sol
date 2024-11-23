8f37ac786df193d1ca374dff634d56d0addac24d
Vaishali Shivakumar <130084250+Vaishu1293@users.noreply.github.com>
2024-11-23 21:23:06 +0000

    nft-smart-contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,18 +1,17 @@
-//  SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-//  INTERNAL IMPORT FOR NFT OPENZEPLIN
+// INTERNAL IMPORT FOR NFT OPENZEPLIN
 import "@openzeppelin/contracts/utils/Counters.sol";
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
-contract NFTMarketPlace is ECR721URIStorage{
-    using Counters for Counters.counter;
+contract NFTMarketPlace is ERC721URIStorage {
+    using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIds;
-    Counters.counter private _itemsSold;
+    Counters.Counter private _itemsSold;
 
     uint256 listingPrice = 0.0025 ether;
 
@@ -28,7 +27,7 @@ contract NFTMarketPlace is ECR721URIStorage{
         bool sold;
     }
 
-    event idMarketItemCreated(
+    event MarketItemCreated(
         uint256 indexed tokenId,
         address seller,
         address owner,
@@ -36,25 +35,32 @@ contract NFTMarketPlace is ECR721URIStorage{
         bool sold
     );
 
-    modifier onlyOwner {
-        require(msg.sender === owner, "only owner of the marketplace can change the listing price");
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the owner of the marketplace can change the listing price"
+        );
+        _; // Required to execute the rest of the function
     }
 
-    constructor() ERC721("NFT Metavarse Token", "MYNFT"){
-        owner == payable(msg.sender);
+    constructor() ERC721("NFT Metaverse Token", "MYNFT") {
+        owner = payable(msg.sender); // Correct owner assignment
     }
 
     function updateListingPrice(uint256 _listingPrice) public payable onlyOwner {
-       listingPrice = _listingPrice;
+        listingPrice = _listingPrice;
     }
 
-    function getListingPrice() public view returns(uint256) {
+    function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
 
-    //  Let create "CREATE NFT TOKEN FUNCTION"
-
-    function createToken(string memory tokenURI, uint256 price) public payable returns(uint256) {
+    // CREATE NFT TOKEN FUNCTION
+    function createToken(string memory tokenURI, uint256 price)
+        public
+        payable
+        returns (uint256)
+    {
         _tokenIds.increment();
 
         uint256 newTokenId = _tokenIds.current();
@@ -67,11 +73,13 @@ contract NFTMarketPlace is ECR721URIStorage{
         return newTokenId;
     }
 
-    //  CREATING MARKET ITEMS
-
+    // CREATE MARKET ITEMS
     function createMarketItem(uint256 tokenId, uint256 price) private {
-        require(price > 0,  "Price must be atleast 1");
-        require(msg.value == listingPrice, "Price must be equal to listing price");
+        require(price > 0, "Price must be at least 1");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to the listing price"
+        );
 
         idMarketItem[tokenId] = MarketItem(
             tokenId,
@@ -85,9 +93,4 @@ contract NFTMarketPlace is ECR721URIStorage{
 
         emit MarketItemCreated(tokenId, msg.sender, address(this), price, false);
     }
-
-    //  FUNCTION FOR RESALE TOKEN
-
-    // Left Video 2 in the laylist at 31:46  https://youtu.be/vbs-_cVWXjY?si=_B44YAeqtJSte2nS
-
 }
